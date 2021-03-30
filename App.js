1a417ed7a058c3fe60bd2ea45d3551fa7d48b0ac
Norman Nolasco <nnolasco@advarion.com>
2021-03-30 04:47:19 -0500

    clean installs

--- App.js
+++ App.js
@@ -5,10 +5,14 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './src/hooks/useCachedResources';
 import useColorScheme from './src/hooks/useColorScheme';
 import Navigation from './src/navigation';
+import * as data from './contentConfig.json';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  const word = data.testing;
+  console.log(word); // output 'testing'
 
   if (!isLoadingComplete) {
     return null;
